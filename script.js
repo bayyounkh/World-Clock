f7ab81957c38ad1931145285533cd328e40b10a7
Bayan <65549175+bayyounkh@users.noreply.github.com>
2022-11-22 11:10:54 +0100

    added current time for the cities in the  drop menu

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function updateCurrentTime() {
   let hamburgCurrentTime = moment().tz('Europe/Hamburg');
 
   hamburgDate.innerHTML = hamburgCurrentTime.format('MMMM Do YYYY');
-  hamburgTime.innerHTML = hamburgCurrentTime.format('h:mm:ss');
+  hamburgTime.innerHTML = hamburgCurrentTime.format('HH:mm:ss');
   //New York
   let newYorkElement = document.querySelector('#new-york');
   let newYorkDate = newYorkElement.querySelector('.date');
@@ -14,7 +14,7 @@ function updateCurrentTime() {
   let newYorkCurrentTime = moment().tz('America/New_York');
 
   newYorkDate.innerHTML = newYorkCurrentTime.format('MMMM Do YYYY');
-  newYorkTime.innerHTML = newYorkCurrentTime.format('h:mm:ss');
+  newYorkTime.innerHTML = newYorkCurrentTime.format('HH:mm:ss');
 
   //Damascus
   let damascusElement = document.querySelector('#damascus');
@@ -23,7 +23,29 @@ function updateCurrentTime() {
   let damascusCurrentTime = moment().tz('Asia/Damascus');
 
   damascusDate.innerHTML =  damascusCurrentTime.format('MMMM Do YYYY');
-  damascusTime.innerHTML = damascusCurrentTime.format('h:mm:ss');
+  damascusTime.innerHTML = damascusCurrentTime.format('HH:mm:ss');
 }
+
+function addCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace('_', ' ').split('/')[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let cities = document.querySelector("#cities");
+
+    cities.innerHTML = `<div class='city'>
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("HH:mm:ss")}</div>
+    </div>`;
+
+}
+
 updateCurrentTime();
 setInterval(updateCurrentTime, 1000);
+
+let citiesSelect = document.querySelector("#city");
+citiesSelect.addEventListener("change", addCity);
+addCity();
+setInterval(addCity, 1000);
